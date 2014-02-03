627adb794f22321570dd652059587ab2c62cd032
abubeck <alexander.bubeck@ipa.fraunhofer.de>
2014-02-03 17:38:06 +0100

    Trimmed the branch to only the bride_compilers sub directory

--- bride_base/org.best_of_robotics.transform.service/src/org/best_of_robotics/transform/service/parameter/ITransformParameter.java
+++
@@ -1,9 +0,0 @@
-package org.best_of_robotics.transform.service.parameter;
-
-public interface ITransformParameter {
-	
-	public void setTransformName(String transformName);
-	
-	public String getTransformName();
-
-}
