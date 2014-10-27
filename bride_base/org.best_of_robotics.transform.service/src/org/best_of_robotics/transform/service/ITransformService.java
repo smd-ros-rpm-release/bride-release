8aa4092af86e5f0f5d03fad2b3a71a2857a998c3
Alexander Bubeck <alexander.bubeck@ipa.fraunhofer.de>
2014-10-27 08:50:23 +0100

    Trimmed the branch to only the bride_tutorials sub directory

--- bride_base/org.best_of_robotics.transform.service/src/org/best_of_robotics/transform/service/ITransformService.java
+++
@@ -1,9 +0,0 @@
-package org.best_of_robotics.transform.service;
-
-import org.best_of_robotics.transform.service.parameter.ITransformParameter;
-
-public interface ITransformService {
-	
-	public void doTransform(ITransformParameter transformParameter);
-
-}
